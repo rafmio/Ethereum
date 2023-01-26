db372c2675c4844bdc01eabbbaf40fb9e56dfc8a
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-01-26 23:13:10 +0300

    Home PC 26.01.2023 23:12

---
+++ src/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "log"
+
+  "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+  client, err := ethclient.Dial("https://cloudflare-eth.com")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println("we have a connection")
+  _ = client
+}
+
+
+// func Dial(rewurl string) (*Client, error)
+// Dial connects a client to the given URL
+// If you hae a local instance of geth you may pass the path to the IPC endpoint
+// client, err := ethclient.Dial("/home/user/.ethereum/geth.ipc")
