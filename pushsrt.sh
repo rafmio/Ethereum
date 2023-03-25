050161fd26e59c0c9fed8c8be6636cb0657ac7be
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-03-25 19:32:39 +0300

    STRINGTOCOMMIT

--- pushsrt.sh
+++ pushsrt.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-git pull
+git pull > /div/null
 STATUS=$(git status)
 UPTODATE="nothing to commit, working tree clean"
 if [[ $STATUS == *"$UPTODATE"* ]]; then
 	echo "All data is updated" > /dev/null
+else
+	git add .
+	DATETIME=$(date+'%d.%m.%Y %H:%M')
+	STRINGTOCOMMIT="Home PC $DATETIME"
+	git commit -m "STRINGTOCOMMIT"
+	git push
 fi
 
